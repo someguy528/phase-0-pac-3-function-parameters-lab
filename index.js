0843abe144376352f1d8a3ecd4a1b4a1ec88c7de
Brandon Castro <castroyou999@gmail.com>
2022-07-17 20:46:38 -0400

    commit tree aa6bd0491c1ffd7bd2968b9054f58c685e53639d

---
+++ index.js
@@ -0,0 +1,10 @@
+
+function introduction(name) {
+    return (`Hi, my name is ${name}.`);
+}
+function introductionWithLanguage(name = "User", language) {
+    return (`Hi, my name is ${name} and I am learning to program in ${language}.`);
+}
+function introductionWithLanguageOptional(name = "User", language = "JavaScript") {
+    return (`Hi, my name is ${name} and I am learning to program in ${language}.`);
+}
